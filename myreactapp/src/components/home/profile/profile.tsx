cae2bd6d7bcb494ff52971febe0e5f5693a99524
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2024-01-26 12:14:27 +0545

    change format of how we recive data from server

--- myreactapp/src/components/home/profile/profile.tsx
+++ myreactapp/src/components/home/profile/profile.tsx
@@ -1,60 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState , useEffect} from "react";
 import axios from 'axios';
 import Cookies from "js-cookie";
 
-interface ProfileData {
-    id :number,
-    username: string,
-    firstname:string,
-    lastname:string,
-    email:string,
-    phonenumber:string,
-    address:string,
-    gender:string,
-    dateofbirth:string
-
-}
 
 const Profile: React.FC = () => {
+    /* 
+    This is profile section.
+    All data displayed in profile are done thorugh here.
+    */
     
+    // setting interface for my cookie data
     interface cookieData{
         key1:string
     }
 
-    const [data, setData] = useState<any[]>([]);
-    const sendDataToDjango = async(data:cookieData): Promise<object> => {
-        const url = '/api/profile/'
-        try{
-            const response = await axios.post(url, data);
-            console.log('Data sent successfully:');
-            return response.data;
-
-        }
-        catch (error) {
-            console.error('Error sending data:', error);
-            return {key1:"error"}
-          }
-    }
+    // assigning the cookie data to my inerface
     const senddata: cookieData ={
         key1: Cookies.get()['username']
     }
-    const responseData =sendDataToDjango(senddata)
-    // setData(responseData)
-    // console.log(responseData)
-    responseData.then((data) =>{
-        // if(typeof data === "object"){
 
-            // for(const key in data){
-            //     console.log(key)
-            // }
-        // }
-        // console.log(data)
-
-        console.log(data)
-    })
+    // creating a state to store data which i get form server
+    const [data, setData] = useState<any[]>([]);
+    
+    
+    // sending post request to server
+    useEffect(() => {
+        axios.post('/api/profile/',senddata)
+        .then(response =>{ setData(response.data)})
+          
+          .catch(error => {
+            console.error('Error fetching data:', error);
+          });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);
+   
 
   console.log(data)
-//   console.log(Cookies.get()['username'])
     return(
         <div>
             <h1>Profile Page</h1>
