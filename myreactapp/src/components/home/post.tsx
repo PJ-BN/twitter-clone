97831a80138ed8dc55458ee460d2ccbe3aa08334
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2024-01-28 20:26:13 +0545

    Update tweet posting component and settings file

--- myreactapp/src/components/home/post.tsx
+++ myreactapp/src/components/home/post.tsx
@@ -1,34 +1,41 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-import React, { useState } from "react";
 
-interface tweet  {
-    date :Date| null;
-    content:string| null;
-    user:string| undefined;
+// Define the shape of a tweet object
+interface Tweet {
+  date: Date | null;
+  content: string | null;
+  user: string | undefined;
 }
+
+// Define the Post component that sends a tweet to the server
 const Post = (tweet: any) => {
-    const date =new Date(tweet['id'])
-    // const [response, setResponse] = useState()
-    
-    const data:tweet = {
-        date:date,
-        content:tweet['content'],
-        user:Cookies.get("username")
-    }
-    
-    console.log(data)
-    try {
-        axios.post('/api/tweet/',data)
-        .then(response =>{console.log(response.data)})
+  // Convert the tweet id to a Date object
+  const date = new Date(tweet['id']);
 
-        
-    } catch (error) {
-        console.log('Error!')
-        
-    } 
-   
+  // Create a tweet object with the necessary data
+  const data: Tweet = {
+    date: date,
+    content: tweet['content'],
+    user: Cookies.get("username") // Get the username from the cookies
+  };
 
+  // Log the tweet data for debugging purposes
+  console.log(data);
+
+  // Send a POST request to the server with the tweet data
+  try {
+    axios.post('/api/tweet/', data)
+      .then(response => {
+        // Log the server response for debugging purposes
+        console.log(response.data);
+      });
+
+  } catch (error) {
+    // Log an error message if the request fails
+    console.log('Error!');
+  }
 }
 
+// Export the Post component as the default export
 export default Post;
