c866da4a6fe4cc700b7268450e6fb911c4f2eb19
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2024-02-09 16:07:32 +0545

    Add search bar and user profiles to message component

--- myreactapp/src/components/home/messages/messageUser.tsx
+++ myreactapp/src/components/home/messages/messageUser.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React , {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Import faSearch icon
 import "./message.css"
 
 interface MessageProps {
@@ -7,12 +9,85 @@ interface MessageProps {
     timestamp: string;
 }
 
+
 const Message: React.FC<MessageProps> = ({ content, sender, timestamp }) => {
+
+    const tweet = {
+        id: 1,
+        name: "Luffy",
+        username: "luffy",
+        content: "I'm gonna be the king of the pirates",
+        date: "2021-08-02",
+        avatar: "luffy.jpg"
+    }
+    
     return (
         <div className="message">
-            <div className="message-sender">{sender}</div>
+            {/* <div className="message-sender">{sender}</div>
             <div className="message-content">{content}</div>
-            <div className="message-timestamp">{timestamp}</div>
+            <div className="message-timestamp">{timestamp}</div> */}
+
+            <div className='top-section'>
+                <b>Messages</b>
+                {/* Setting icon */}
+                <FontAwesomeIcon icon={faCog} />
+                {/* New mail icon */}
+                <FontAwesomeIcon icon={faEnvelope} />
+                {/* Search icon */}
+
+            </div>
+
+            <div className='top-search-bar'>
+                <input type="text"  className='search-bar' placeholder=" Search Profile"/>
+            </div>
+
+            <div className='message-user'>
+                <nav>
+
+                    <div key={tweet.id} className="tweet-section">
+                        <div className="post post-tweet">
+                            <div className="profile-pic">
+                                <img
+                                src={process.env.PUBLIC_URL + "luffy.jpg"}
+                                alt="profile-pic"
+                                className="profile-pic"
+                                />
+                            </div>
+                            <div className="tweet-content">
+
+                                    <div className="tweet-user">
+
+                                        <b>{tweet.name}</b>
+                                        <span className='span-username'>@{tweet.username} .</span>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div key={tweet.id} className="tweet-section">
+                        <div className="post post-tweet">
+                            <div className="profile-pic">
+                                <img
+                                src={process.env.PUBLIC_URL + "luffy.jpg"}
+                                alt="profile-pic"
+                                className="profile-pic"
+                                />
+                            </div>
+                            <div className="tweet-content">
+
+                                    <div className="tweet-user">
+
+                                        <b>{tweet.name}</b>
+                                        <span className='span-username'>@{tweet.username} .</span>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+
+
+            </div>
+
         </div>
     );
 };
